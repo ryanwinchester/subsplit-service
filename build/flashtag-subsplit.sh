c1c2accfb0d46fba270b5b7ac2215f8be4dd8e7a
ryanwinchester <fungku@gmail.com>
2015-12-11 23:58:46 -0800

    add develop branch

--- build/flashtag-subsplit.sh
+++ build/flashtag-subsplit.sh
@@ -1,7 +1,7 @@
 git subsplit init git@github.com:flashtag/flashtag.git
-git subsplit publish --heads="master" app/Admin:git@github.com:flashtag/admin.git
-git subsplit publish --heads="master" app/Api:git@github.com:flashtag/api.git
-git subsplit publish --heads="master" app/Front:git@github.com:flashtag/front.git
-git subsplit publish --heads="master" app/Cms:git@github.com:flashtag/cms.git
-git subsplit publish --heads="master" app/Data:git@github.com:flashtag/data.git
+git subsplit publish --heads="master develop" app/Admin:git@github.com:flashtag/admin.git
+git subsplit publish --heads="master develop" app/Api:git@github.com:flashtag/api.git
+git subsplit publish --heads="master develop" app/Front:git@github.com:flashtag/front.git
+git subsplit publish --heads="master develop" app/Cms:git@github.com:flashtag/cms.git
+git subsplit publish --heads="master develop" app/Data:git@github.com:flashtag/data.git
 rm -rf .subsplit/
