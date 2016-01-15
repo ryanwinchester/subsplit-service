edc89c0922126ca62dbb8b7e10826ad75d0ebdd3
ryanwinchester <fungku@gmail.com>
2016-01-14 23:49:32 -0800

    Remove cms package from split

--- build/flashtag-subsplit.sh
+++ build/flashtag-subsplit.sh
@@ -2,6 +2,5 @@ git subsplit init git@github.com:flashtag/flashtag.git
 git subsplit publish --heads="master develop" app/Admin:git@github.com:flashtag/admin.git
 git subsplit publish --heads="master develop" app/Api:git@github.com:flashtag/api.git
 git subsplit publish --heads="master develop" app/Front:git@github.com:flashtag/front.git
-git subsplit publish --heads="master develop" app/Cms:git@github.com:flashtag/cms.git
 git subsplit publish --heads="master develop" app/Data:git@github.com:flashtag/data.git
 rm -rf .subsplit/
