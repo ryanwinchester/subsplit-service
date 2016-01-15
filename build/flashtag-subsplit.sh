d1df678bde1c3bbfaaa0c4a793f953199eb03c4d
ryanwinchester <fungku@gmail.com>
2016-01-14 23:45:45 -0800

    switch ownership again of flashtag

--- build/flashtag-subsplit.sh
+++ build/flashtag-subsplit.sh
@@ -1,4 +1,4 @@
-git subsplit init git@github.com:sevenshores/flashtag.git
+git subsplit init git@github.com:flashtag/flashtag.git
 git subsplit publish --heads="master develop" app/Admin:git@github.com:flashtag/admin.git
 git subsplit publish --heads="master develop" app/Api:git@github.com:flashtag/api.git
 git subsplit publish --heads="master develop" app/Front:git@github.com:flashtag/front.git
