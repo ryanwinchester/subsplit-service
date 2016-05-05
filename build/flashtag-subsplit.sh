b9a357a5715e76edd1fd3c229cadb2f66371c5d9
ryanwinchester <fungku@gmail.com>
2016-05-05 11:28:56 -0700

    update split source

--- build/flashtag-subsplit.sh
+++ build/flashtag-subsplit.sh
@@ -1,6 +1,6 @@
 WORK_DIR="$1"
 
-git subsplit --work-dir="$WORK_DIR" init git@github.com:flashtag/flashtag.git
+git subsplit --work-dir="$WORK_DIR" init git@github.com:flashtag/development.git
 git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Admin:git@github.com:flashtag/admin.git
 git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Api:git@github.com:flashtag/api.git
 git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Front:git@github.com:flashtag/front.git
