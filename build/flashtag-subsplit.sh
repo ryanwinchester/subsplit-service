76af6fa65fee62c914e00067c9653d4ed144476f
ryanwinchester <fungku@gmail.com>
2016-08-05 14:50:17 -0700

    update subsplit

--- build/flashtag-subsplit.sh
+++ build/flashtag-subsplit.sh
@@ -1,10 +1,11 @@
 WORK_DIR="$1"
 
 git subsplit --work-dir="$WORK_DIR" init git@github.com:flashtag/development.git
-git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Admin:git@github.com:flashtag/admin.git
-git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Api:git@github.com:flashtag/api.git
-git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Front:git@github.com:flashtag/front.git
-git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Data:git@github.com:flashtag/data.git
-git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/Core:git@github.com:flashtag/core.git
+git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/admin:git@github.com:flashtag/admin.git
+git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/api:git@github.com:flashtag/api.git
+git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/auth:git@github.com:flashtag/auth.git
+git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/front:git@github.com:flashtag/front.git
+git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/posts:git@github.com:flashtag/posts.git
+git subsplit publish --work-dir="$WORK_DIR" --heads="master develop" app/core:git@github.com:flashtag/core.git
 
 rm -rf "$WORK_DIR"
